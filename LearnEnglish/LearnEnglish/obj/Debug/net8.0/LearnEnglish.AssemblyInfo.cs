d7a1f1c2f7769f6b042809f84f8000bb9986c7b9
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-06-28 13:00:06 -0300

    Botões funcionais e opções

--- LearnEnglish/LearnEnglish/obj/Debug/net8.0/LearnEnglish.AssemblyInfo.cs
+++ LearnEnglish/LearnEnglish/obj/Debug/net8.0/LearnEnglish.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnEnglish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27c2576451100a0ccce3f2bddfaf62667c3b48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnEnglish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnEnglish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
